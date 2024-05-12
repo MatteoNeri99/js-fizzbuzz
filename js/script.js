55400ded8a22f21be6110b45ae45dc1eda197254
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-12 19:06:14 +0200

    creata istruzione condizionale

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ for (let i=0; i<100; i++ ){
 
     let myNumero= i + 1;
      
-   
-    console.log(myNumero)
+    
+    if (myNumero %3===0 && myNumero %5===0){
+        console.log("FizzBuzz")
+    }else if (myNumero %5===0){
+        console.log("Buzz")
+    }else if (myNumero %3===0){
+        console.log("Fizz")
+    }else{
+        console.log(myNumero)
+    }
 }
